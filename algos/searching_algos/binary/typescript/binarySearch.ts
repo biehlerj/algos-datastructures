fc04ab81f2f8c6aa8af950e9602047f360c210a6
Jacob Biehler <biehlerj@protonmail.com>
2021-07-14 16:43:18 -0700

    Adding jsdoc to function

--- algos/searching_algos/binary/typescript/binarySearch.ts
+++ algos/searching_algos/binary/typescript/binarySearch.ts
@@ -1,3 +1,9 @@
+/**
+ * @param {Array<number>} arr - The array to search
+ * @param {number}        arrLen - The length of the array to search
+ * @param {number}        target - The item to find in the array
+ * @return {number}       The index of the target or -1 if the target isn't present
+ */
 export function binarySearch(arr: Array<number>, arrLen: number, target: number): number {
     let left = 0;
     let right = arrLen - 1;
