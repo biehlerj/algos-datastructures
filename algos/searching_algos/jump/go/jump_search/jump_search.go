9d9c597ad24e62786c044cd33ffe446dff0f3b56
Jacob Biehler <biehlerj@protonmail.com>
2021-08-12 17:25:28 -0700

    Adding skeleton file for jump search in Java and adding wikipedia link to go file

--- algos/searching_algos/jump/go/jump_search/jump_search.go
+++ algos/searching_algos/jump/go/jump_search/jump_search.go
@@ -4,7 +4,7 @@ import "math"
 
 // JumpSearch searches an array of n length looking for the given target
 // It does so following the jump search algorithm:
-// TODO wikipedia link
+// https://en.wikipedia.org/wiki/Jump_search
 // It return the index where the target is or -1 if the target isn't present
 func JumpSearch(arr []int64, arrLen int64, target int64) int64 {
 	var lower int64 = 0
---
+++ algos/searching_algos/jump/java/JumpSearch.java
@@ -0,0 +1,20 @@
+/**
+ * Implementation of binary search algorithm in Java
+ *
+ * <p>
+ * The performance is O(√n)
+ * </p>
+ *
+ * @author Jacob Biehler (https://sr.ht/~biehlerj/)
+ */
+class JumpSearch {
+        /**
+         * @param arr is the array to search
+         * @param arrLen is the length of the array
+         * @param target is the item to find
+         * @return index of the element or -1 if not found
+        */
+        int jumpSearch(int arr[], int arrLen, int target) {
+          
+        }
+}
